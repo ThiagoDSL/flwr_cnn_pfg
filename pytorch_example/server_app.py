6249d83cb271249edcaf2a140629b804fe1c835f
ThiagoDSL <thiago.danilo4@gmail.com>
2024-12-10 22:14:13 -0300

    Fixing my dumb git

---
+++ pytorch_example/server_app.py
@@ -0,0 +1,89 @@
+"""pytorch-example: A Flower / PyTorch app."""
+
+import torch
+from pytorch_example.strategy import CustomFedAvg
+from pytorch_example.task import (
+    Net,
+    get_weights,
+    set_weights,
+    validate,
+    load_data,
+)
+from torch.utils.data import DataLoader
+from pytorch_example.task import load_test_data
+from datasets import load_dataset
+from flwr.common import Context, ndarrays_to_parameters
+from flwr.server import ServerApp, ServerAppComponents, ServerConfig
+
+
+def gen_evaluate_fn(
+    valloader: DataLoader,
+    device: torch.device,
+):
+    """Generate the function for centralized evaluation."""
+
+    def evaluate(server_round, parameters_ndarrays, config):
+        """Evaluate global model on centralized test set."""
+        net = Net()
+        set_weights(net, parameters_ndarrays)
+        net.to(device)
+        loss, accuracy = validate(net, valloader, device=device)
+        return loss, {"centralized_accuracy": accuracy}
+
+    return evaluate
+
+
+def on_fit_config(server_round: int):
+    """Construct `config` that clients receive when running `fit()`"""
+    lr = 0.1
+    # Enable a simple form of learning rate decay
+    if server_round > 10:
+        lr /= 2
+    return {"lr": lr}
+
+
+# TODO: change this
+# Define metric aggregation function
+def weighted_average(metrics):
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
+
+    # Aggregate and return custom metric (weighted average)
+    return {"federated_evaluate_accuracy": sum(accuracies) / sum(examples)}
+
+
+def server_fn(context: Context):
+    # Read from config
+    num_rounds = context.run_config["num-server-rounds"]
+    fraction_fit = context.run_config["fraction-fit"]
+    fraction_eval = context.run_config["fraction-evaluate"]
+    server_device = context.run_config["server-device"]
+    batch_size = context.run_config["batch-size"]
+
+    # Initialize model parameters
+    ndarrays = get_weights(Net())
+    parameters = ndarrays_to_parameters(ndarrays)
+
+    # Prepare dataset for central evaluation
+    # THE TEST DATA IS SEEN BY THE MODEL ONLY ONCE, FOR FINAL EVALUATION
+    testloader = load_test_data(batch_size=batch_size)
+
+    # Define strategy
+    strategy = CustomFedAvg(
+        run_config=context.run_config,
+        use_wandb=context.run_config["use-wandb"],
+        fraction_fit=fraction_fit,
+        fraction_evaluate=fraction_eval,
+        initial_parameters=parameters,
+        on_fit_config_fn=on_fit_config,
+        evaluate_fn=gen_evaluate_fn(testloader, device=server_device),
+        evaluate_metrics_aggregation_fn=weighted_average,
+    )
+    config = ServerConfig(num_rounds=num_rounds)
+
+    return ServerAppComponents(strategy=strategy, config=config)
+
+
+# Create ServerApp
+app = ServerApp(server_fn=server_fn)
---
+++ pytorch_example/task.py
@@ -0,0 +1,251 @@
+"""pytorch-example: A Flower / PyTorch app."""
+
+import json
+from collections import OrderedDict
+from datetime import datetime
+from pathlib import Path
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+from flwr_datasets import FederatedDataset
+from flwr_datasets.partitioner import DirichletPartitioner
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import (
+    Compose,
+    Normalize,
+    RandomCrop,
+    RandomHorizontalFlip,
+    RandomVerticalFlip,
+    RandomAffine,
+    ToTensor,
+    Resize,
+    Grayscale,
+)
+
+from flwr.common.typing import UserConfig
+
+FM_NORMALIZATION = ((0.1307,), (0.3081,))
+TEST_TRANSFORMS = Compose([ToTensor(), Normalize(*FM_NORMALIZATION)])
+TRAIN_TRANSFORMS = Compose(
+    [
+        Grayscale(num_output_channels=1),
+        Resize((32, 32)),
+        RandomCrop(32, padding=4),
+        RandomHorizontalFlip(),
+        RandomVerticalFlip(),
+        RandomAffine(degrees=10, shear=0.1),
+        ToTensor(),
+        Normalize(*FM_NORMALIZATION),
+    ]
+)
+
+
+# class TrafficSignModel(nn.Module):
+class Net(nn.Module):
+    """Model (simple CNN adapted for Fashion-MNIST)"""
+    
+    # We neet to specify the images format and the number of classes, this can be delegated
+    # to the configuration files -> context.dimensions smth smth
+    def __init__(self):
+        super(Net, self).__init__()
+
+        # 32x32 -> Conv -> Relu -> BN -> POOL -> 16x16
+        # Dimensions: (Channels, Height, Width)
+        # self.conv1 = nn.Conv2d(in_channels=dimensions[0], out_channels=32, kernel_size=(4,4), padding=2)  # padding=2 for 'same' padding
+        # in_channels=1 means grayscale, out_channels=32 means image is 32x32
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=(4,4), padding=2)  # padding=2 for 'same' padding
+        self.bn1 = nn.BatchNorm2d(32)
+        self.pool1 = nn.MaxPool2d(kernel_size=(2,2))
+
+        # 16x16 -> (Conv -> Relu -> BN)*2 -> 8x8 
+        self.conv2_1 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(4,4), padding=2)
+        self.bn2_1 = nn.BatchNorm2d(64)
+        self.conv2_2 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=4, padding=2)
+        self.bn2_2 = nn.BatchNorm2d(64)
+        self.pool2 = nn.MaxPool2d(kernel_size=2)
+
+        # 8x8 -> (Conv -> Relu -> BN)*3 -> 4x4
+        self.conv3_1 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=4, padding=2)
+        self.bn3_1 = nn.BatchNorm2d(128)
+        self.conv3_2 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=4, padding=2)
+        self.bn3_2 = nn.BatchNorm2d(128)
+        self.conv3_3 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=4, padding=2)
+        self.bn3_3 = nn.BatchNorm2d(128)
+        self.pool3 = nn.MaxPool2d(kernel_size=2)
+
+        # self.fc1 = nn.Linear(in_features=128 * (dimensions[1] // 8) * (dimensions[2] // 8), out_features=128)
+        self.fc1 = nn.Linear(in_features=128*4*4, out_features=128)
+        self.bn_fc1 = nn.BatchNorm1d(128)
+        self.dropout1 = nn.Dropout(0.7)
+
+        self.fc2 = nn.Linear(128, 128)
+        self.bn_fc2 = nn.BatchNorm1d(128)
+        self.dropout2 = nn.Dropout(0.5)
+
+        # GTSRB 43
+        num_classes = 43
+        self.fc3 = nn.Linear(128, num_classes)
+
+    def forward(self, x):
+        x = self.pool1(self.bn1(F.relu(self.conv1(x))))
+
+        x = self.bn2_1(F.relu(self.conv2_1(x)))
+        x = self.pool2(self.bn2_2(F.relu(self.conv2_2(x))))
+
+        x = self.bn3_1(F.relu(self.conv3_1(x)))
+        x = self.bn3_2(F.relu(self.conv3_2(x)))
+        x = self.pool3(self.bn3_3(F.relu(self.conv3_3(x))))
+
+        x = torch.flatten(x, 1)
+
+        x = self.bn_fc1(F.relu(self.fc1(x)))
+        x = self.dropout1(x)
+
+        x = self.bn_fc2(F.relu(self.fc2(x)))
+        x = self.dropout2(x)
+
+        x = self.fc3(x)
+
+        return F.softmax(x, dim=1)
+
+
+def train(net, trainloader, epochs, lr, device):
+    """Train the model on the training set."""
+    net.to(device)  # move model to GPU if available
+    
+    # trainloader handles one-hot encoding automatically
+    # data augmentation was done on trainloader
+    criterion = torch.nn.CrossEntropyLoss().to(device)
+    optimizer = optim.Adam(net.parameters(), lr=lr)
+    
+    net.train()
+    running_loss = 0.0
+    for _ in range(epochs):
+        for batch in trainloader:
+            images = batch["image"]
+            labels = batch["label"]
+            optimizer.zero_grad()
+            loss = criterion(net(images.to(device)), labels.to(device))
+            loss.backward()
+            optimizer.step()
+            running_loss += loss.item()
+
+    # TODO: search more about this
+    # avg train loss might not be the best one for our purpose
+    avg_trainloss = running_loss / len(trainloader)
+    return avg_trainloss
+
+
+def validate(net, valloader, device):
+    """Validate the model on the validation set."""
+    # net.to(device)
+    net.eval()
+    correct, loss = 0, 0.0
+    criterion = torch.nn.CrossEntropyLoss()#.to(device)
+    with torch.no_grad():
+        for batch in valloader:
+            images = batch["image"].to(device)
+            labels = batch["label"].to(device)
+            outputs = net(images)
+            loss += criterion(outputs, labels).item()
+            correct += (torch.max(outputs.data, 1)[1] == labels).sum().item()
+    accuracy = correct / len(valloader.dataset)
+    loss = loss / len(valloader)
+    return loss, accuracy
+
+def test(net, testloader, device):
+    net.eval()
+    correct = 0
+
+    with torch.no_grad():
+        for batch in testloader:
+            images = batch["image"].to(device)
+            labels = batch["label"].to(device)
+            outputs = net(images)
+            correct += (torch.max(outputs, 1)[1] == labels).sum().item()
+    accuracy = correct / len(testloader.dataset)
+    return accuracy
+
+
+def get_weights(net):
+    return [val.cpu().numpy() for _, val in net.state_dict().items()]
+
+
+def set_weights(net, parameters):
+    params_dict = zip(net.state_dict().keys(), parameters)
+    state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+    net.load_state_dict(state_dict, strict=True)
+
+
+def apply_train_transforms(batch):
+    """Apply transforms to the partition from FederatedDataset."""
+    batch["image"] = [TRAIN_TRANSFORMS(img) for img in batch["image"]]
+    return batch
+
+
+def apply_eval_transforms(batch):
+    """Apply transforms to the partition from FederatedDataset."""
+    batch["image"] = [TEST_TRANSFORMS(img) for img in batch["image"]]
+    return batch
+
+
+fds = None  # Cache FederatedDataset
+
+
+def load_data(partition_id: int, num_partitions: int, batch_size: int):
+    """Load partition FashionMNIST data."""
+    # Only initialize `FederatedDataset` once
+    global fds
+    if fds is None:
+        train_dataset = datasets.GTSRB(
+            root="data", split="train", download=True, transform=TRAIN_TRANSFORMS
+        )
+        partitioner = DirichletPartitioner(
+            num_partitions=num_partitions,
+            partition_by="label",
+            alpha=0.5,
+            seed=42,
+        )
+        fds = FederatedDataset(
+            train_dataset,
+            partitioner.partition(),
+        )
+    
+    partition = fds.load_partition(partition_id)
+    # Divide data on each node: 80% train, 20% test
+    partition_train_val = partition.train_test_split(test_size=0.1, seed=42)
+
+    train_partition = partition_train_val["train"]
+    val_partition = partition_train_val["test"]
+    
+    trainloader = DataLoader(train_partition, batch_size=batch_size, shuffle=True)
+    valloader = DataLoader(val_partition, batch_size=batch_size, shuffle=True)
+    return trainloader, valloader
+
+
+def load_test_data(batch_size: int):
+    test_dataset = datasets.GTSRB(
+        root="data", split="test", download=True, transform=TEST_TRANSFORMS 
+    )
+    testloader = DataLoader(test_dataset, batch_size=batch_size)
+    return testloader
+
+
+def create_run_dir(config: UserConfig) -> Path:
+    """Create a directory where to save results from this run."""
+    # Create output directory given current timestamp
+    current_time = datetime.now()
+    run_dir = current_time.strftime("%Y-%m-%d/%H-%M-%S")
+    # Save path is based on the current directory
+    # Assumes Linux / Git Bash style commands
+    save_path = Path.cwd() / f"outputs/{run_dir}"
+    save_path.mkdir(parents=True, exist_ok=False)
+
+    # Save run config as json
+    with open(f"{save_path}/run_config.json", "w", encoding="utf-8") as fp:
+        json.dump(config, fp)
+
+    return save_path, run_dir
